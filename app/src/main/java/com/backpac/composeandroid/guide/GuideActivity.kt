f163443928f29233fd4c1f3f7a8498b74f1c8ce6
Leekangmin <kkk5474096@naver.com>
2022-06-18 15:36:12 +0900

    이미지 테두리 컬러 적용

--- app/src/main/java/com/backpac/composeandroid/guide/GuideActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/guide/GuideActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,6 +42,7 @@ private fun MessageCard(msg: Message) {
             Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
