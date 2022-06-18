9417faaa77fcfd6b9e861953c70bd8ab833266ea
Leekangmin <kkk5474096@naver.com>
2022-06-18 15:40:28 +0900

    텍스트 배경 폰트 컬러 적용

--- app/src/main/java/com/backpac/composeandroid/guide/GuideActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/guide/GuideActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +19,7 @@ import androidx.compose.ui.unit.dp
 import com.backpac.composeandroid.R
 import com.backpac.composeandroid.ui.theme.ComposeAndroidTheme
 
-class GuideActivity: ComponentActivity() {
+class GuideActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,23 +38,29 @@ data class Message(
 @Composable
 private fun MessageCard(msg: Message) {
     Row(Modifier.padding(all = 8.dp)) {
-        Image(painter = painterResource(id = R.drawable.profile_picture),
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "android",
             Modifier
                 .size(40.dp)
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        
+
         Spacer(modifier = Modifier.width(8.dp))
-        
+
         Column {
-            Text(msg.author)
+            Text(
+                msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(msg.body)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(msg.body, Modifier.padding(all = 4.dp), style = MaterialTheme.typography.body2)
+            }
         }
     }
-
 }
 
 @Preview
