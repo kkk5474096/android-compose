d9dafcc608231f0690f7d35770990d4728ff5f2a
kkk5474096 <kkk5474096@naver.com>
2022-07-27 14:11:15 +0900

    컴포즈 기본적인 뷰

--- app/src/main/java/com/backpac/composeandroid/MainActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.backpac.composeandroid
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -20,7 +17,6 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
---
+++ app/src/main/java/com/backpac/composeandroid/layout/LayoutActivity.kt
@@ -0,0 +1,72 @@
+package com.backpac.composeandroid.layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.backpac.composeandroid.R
+import com.backpac.composeandroid.ui.theme.ComposeAndroidTheme
+
+class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAndroidTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick =  {})
+        .padding(16.dp)) {
+       Surface(
+           modifier = Modifier.size(50.dp),
+           shape = CircleShape,
+           color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+       ) {
+           Image(painter = painterResource(id = R.drawable.profile_picture),
+               contentDescription = "android")
+       }
+        Column(
+            modifier = Modifier.padding(start = 8.dp).align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    ComposeAndroidTheme {
+        MyApp()
+    }
+}
