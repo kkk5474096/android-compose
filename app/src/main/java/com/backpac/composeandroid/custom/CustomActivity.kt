3c3a23450d9f648319d1c9ee3699b3217b415864
kkk5474096 <kkk5474096@naver.com>
2022-07-29 11:18:16 +0900

    MyOwnColumn

--- app/src/main/java/com/backpac/composeandroid/custom/CustomActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/custom/CustomActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -47,11 +48,38 @@ fun Modifier.firstBaselineToTop(
 )
 
 @Composable
-fun CustomLayout(
+fun MyOwnColumn(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map {
+            it.measure(constraints)
+        }
 
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach {
+                it.placeRelative(x = 0, y = yPosition)
+
+                yPosition += it.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
 }
 
 
@@ -67,6 +95,6 @@ fun And() {
 @Composable
 fun Or() {
     ComposeAndroidTheme {
-        Text(text = "Hi there!", Modifier.padding(top = 32.dp))
+        BodyContent()
     }
 }
