fc206d17e0343dc0cb49716e837d2adfeb0de4b4
kkk5474096 <kkk5474096@naver.com>
2022-08-02 11:35:50 +0900

    ConstraintSet Compose

--- app/src/main/java/com/backpac/composeandroid/constraint/ConstraintActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/constraint/ConstraintActivity.kt
@@ -3,13 +3,17 @@ package com.backpac.composeandroid.constraint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.compose.atLeast
 import com.backpac.composeandroid.ui.theme.ComposeAndroidTheme
@@ -69,10 +73,43 @@ fun LargeConstraintLayout() {
     }
 }
 
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraintSet = constraints) {
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.layoutId("button")) {
+                Text("Button")
+            }
+
+            Text(text = "Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin= margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
     ComposeAndroidTheme {
-        LargeConstraintLayout()
+        DecoupledConstraintLayout()
     }
 }
