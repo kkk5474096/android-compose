e93447d090369f0128ddbd96c78f3831410e2e88
kkk5474096 <kkk5474096@naver.com>
2022-08-02 14:45:47 +0900

    내장 기능 활용 Compose

--- app/src/main/java/com/backpac/composeandroid/constraint/ConstraintActivity.kt
+++ app/src/main/java/com/backpac/composeandroid/constraint/ConstraintActivity.kt
@@ -3,11 +3,15 @@ package com.backpac.composeandroid.constraint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -103,6 +107,40 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
         }
         constrain(text) {
             top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    ComposeAndroidTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
         }
     }
 }
